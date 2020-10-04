7805850a21903e5b2c1fcde64cbb3692add3c01c
Mike Sharp <msharp23@hotmail.co.uk>
2020-10-04 16:22:09 +0100

    Just playing with some mocks to figure out budget data structure.

--- src/BudgetDetailView.js
+++ src/BudgetDetailView.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 const BudgetDetailView = (props) => {
-  return <div>Budget detail view for {props.computedMatch.params.id} </div>;
+  const [selectedBudget, setBudget] = useState(null);
+  useEffect(() => {
+    if (!selectedBudget) {
+      setBudget({
+        name: "October 2020",
+        expenses: {
+          fixed: [
+            {
+              name: "Rent",
+              amount: 500,
+              category: "",
+            },
+          ],
+          variable: [
+            {
+              name: "New monitor",
+              amount: 444.44,
+              category: "purchase",
+            },
+          ],
+        },
+        inflows: [
+          {
+            name: "pay",
+            amount: 666.66,
+          },
+          {
+            name: "carry over from last month",
+            amount: 55.23,
+          },
+        ],
+      });
+    }
+  }, [selectedBudget, setBudget]);
+
+  if (!selectedBudget) {
+    return null;
+  }
+  return (
+    <div>
+      <div>Budget detail view for {props.computedMatch.params.id} </div>
+      <h1>{selectedBudget.name}</h1>
+      <pre>{JSON.stringify(selectedBudget)}</pre>
+    </div>
+  );
 };
 
 export default BudgetDetailView;
--- src/DashboardView.js
+++ src/DashboardView.js
@@ -3,23 +3,83 @@ import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./DashboardView.scss";
 const DashboardView = () => {
-  const [currentBudget, setBudget] = useState(null);
+  const [currentBudget, setBudget] = useState({
+    name: "October 2020",
+    expenses: {
+      fixed: [
+        {
+          name: "Rent",
+          amount: 500,
+          category: "",
+        },
+      ],
+      variable: [
+        {
+          name: "New monitor",
+          amount: 444.44,
+          category: "purchase",
+        },
+      ],
+    },
+    inflows: [
+      {
+        name: "pay",
+        amount: 666.66,
+      },
+      {
+        name: "carry over from last month",
+        amount: 55.23,
+      },
+    ],
+  });
   const history = useHistory();
   const [now] = useState(new Date());
-  const cashFlow = 555;
 
+  function calulateTotalFixedInflows(inflows) {
+    if (inflows.length === 1) {
+      return inflows[0].amount;
+    }
+    return inflows.reduce((prev, curr) => prev.amount + curr.amount);
+  }
+  function calulateTotalFixedOutflows(outflows) {
+    if (outflows.length === 1) {
+      return outflows[0].amount;
+    }
+    return outflows.reduce((prev, curr) => prev.amount + curr.amount);
+  }
+  function calulateTotalSpendingOutflows(outflows) {
+    if (outflows.length === 1) {
+      return outflows[0].amount;
+    }
+    return outflows.reduce((prev, curr) => prev.amount + curr.amount);
+  }
+  function calculateTotalOutflows(expenseObject) {
+    return (
+      calulateTotalSpendingOutflows(expenseObject.variable) +
+      calulateTotalFixedOutflows(expenseObject.fixed)
+    );
+  }
+  function calculateCashFlow(budget) {
+    const fixedExpenses = calulateTotalFixedOutflows(budget.expenses.fixed);
+    const variableExpenses = calulateTotalSpendingOutflows(
+      budget.expenses.variable
+    );
+    const inflows = calulateTotalFixedInflows(budget.inflows);
+
+    return Number(inflows - fixedExpenses - variableExpenses);
+  }
   function goToBudgetDetail(budgetId) {
     history.push(`/budget/${budgetId}`);
   }
 
-  useEffect(() => {
-    let loadedBudget = null;
-    const budgets = JSON.parse(localStorage.getItem("budgets"));
-    if (budgets) {
-      loadedBudget = budgets[`${now.getMonth() + 1}-${now.getFullYear()}`];
-      setBudget(loadedBudget);
-    }
-  }, [now]);
+  // useEffect(() => {
+  //   let loadedBudget = null;
+  //   const budgets = JSON.parse(localStorage.getItem("budgets"));
+  //   if (budgets) {
+  //     loadedBudget = budgets[`${now.getMonth() + 1}-${now.getFullYear()}`];
+  //     setBudget(loadedBudget);
+  //   }
+  // }, [now]);
 
   if (!currentBudget) {
     return (
@@ -35,14 +95,23 @@ const DashboardView = () => {
     <div className="DashboardView">
       <h1>Budget summary for {`${now.getMonth() + 1}-${now.getFullYear()}`}</h1>
       <h2>Income</h2>
-      <h4>Total inflows: £2000</h4>
+      <h4>
+        Total inflows: £{calulateTotalFixedInflows(currentBudget.inflows)}
+      </h4>
       <h2>Outgoings</h2>
-      <h4>Total fixed outflows: £500</h4>
-      <h4>Total spending outflows : £1000</h4>
-      <h4>Total outflows: £1500</h4>
+      <h4>
+        Total fixed outflows: £
+        {calulateTotalFixedOutflows(currentBudget.expenses.fixed)}
+      </h4>
+      <h4>
+        Total spending outflows : £
+        {calulateTotalSpendingOutflows(currentBudget.expenses.variable)}
+      </h4>
+      <h4>Total outflows: £{calculateTotalOutflows(currentBudget.expenses)}</h4>
       <h2>Cash flow status</h2>
       <h4>
-        {cashFlow > 0 ? "Positive" : "Negative"} (£{cashFlow})
+        {calculateCashFlow(currentBudget) > 0 ? "Positive" : "Negative"} (£
+        {calculateCashFlow(currentBudget)})
       </h4>
       <button type="button" onClick={() => goToBudgetDetail("foo")}>
         Budget Detail
