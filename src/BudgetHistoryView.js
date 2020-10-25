c96f546875df593f05932eeae56f3fd8d6d85bb6
Mike Sharp <msharp23@hotmail.co.uk>
2020-10-25 22:24:29 +0000

    build dashboard history cards.

--- src/BudgetHistoryView.js
+++ src/BudgetHistoryView.js
@@ -1,25 +1,37 @@
 import "./card.scss";
-
+import { calculateSum } from "./budgetHelpers";
 import React, { useState, useEffect } from "react";
 
 const BudgetHistoryView = () => {
   const [budgetList, setBudgetList] = useState(null);
+
+  function getCashflowStatus(cashflow) {
+    const inflows = calculateSum(cashflow.inflows);
+    const outflows = calculateSum([
+      ...cashflow.expenses.fixed,
+      ...cashflow.expenses.variable,
+    ]);
+    return outflows > inflows ? "Negative" : "Positive";
+  }
   useEffect(() => {
     if (!budgetList) {
-      const budgets = Object.values(JSON.parse(localStorage.budgets));
-      setBudgetList(budgets);
+      if (JSON.parse(localStorage.getItem("budgets"))) {
+        const budgets = Object.values(JSON.parse(localStorage.budgets));
+        setBudgetList(budgets);
+      }
     }
   }, [budgetList, setBudgetList]);
   if (!budgetList) {
-    return <div>No budgets to show</div>;
+    return <div className="BudgetHistory no-budgets">No budgets to show</div>;
   }
   return (
     <div className="BudgetHistory">
       {budgetList.map((budget) => {
         return (
-          <div className="card" key={budget.name}>
-            {budget.name}
-          </div>
+          <div
+            className={`card ${getCashflowStatus(budget)}`}
+            key={budget.name}
+          ></div>
         );
       })}
     </div>
