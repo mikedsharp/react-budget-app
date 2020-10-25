7966cd95f1603d455179c475176c3171e7fe03d1
Mike Sharp <msharp23@hotmail.co.uk>
2020-10-25 18:07:46 +0000

    add: function to create blank budget

--- src/DashboardView.js
+++ src/DashboardView.js
@@ -1,53 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./DashboardView.scss";
 const DashboardView = () => {
-  const [currentBudget] = useState({
-    name: "October 2020",
-    expenses: {
-      fixed: [
-        {
-          name: "Rent",
-          amount: 500,
-          category: "",
-        },
-      ],
-      variable: [
-        {
-          name: "New monitor",
-          amount: 444.44,
-          category: "purchase",
-        },
-      ],
-    },
-    inflows: [
-      {
-        name: "pay",
-        amount: 666.66,
-      },
-      {
-        name: "carry over from last month",
-        amount: 55.23,
-      },
-    ],
-  });
+  const [currentBudget, setBudget] = useState();
   const history = useHistory();
   const [now] = useState(new Date());
 
+  function createBlankBudget() {
+    let budgets = JSON.parse(localStorage.getItem("budgets"));
+    if (!Array.isArray(budgets)) {
+      budgets = {};
+    }
+    budgets[`${now.getMonth() + 1}-${now.getFullYear()}`] = {
+      name: `${now.getMonth() + 1}-${now.getFullYear()}`,
+      expenses: {
+        fixed: [],
+        variable: [],
+      },
+      inflows: [],
+    };
+    localStorage.setItem("budgets", JSON.stringify(budgets));
+    const loadedBudget = budgets[`${now.getMonth() + 1}-${now.getFullYear()}`];
+    setBudget(loadedBudget);
+  }
+
   function calulateTotalFixedInflows(inflows) {
+    if (inflows.length === 0) {
+      return 0;
+    }
     if (inflows.length === 1) {
       return inflows[0].amount;
     }
     return inflows.reduce((prev, curr) => prev.amount + curr.amount);
   }
   function calulateTotalFixedOutflows(outflows) {
+    if (outflows.length === 0) {
+      return 0;
+    }
     if (outflows.length === 1) {
       return outflows[0].amount;
     }
     return outflows.reduce((prev, curr) => prev.amount + curr.amount);
   }
   function calulateTotalSpendingOutflows(outflows) {
+    if (outflows.length === 0) {
+      return 0;
+    }
     if (outflows.length === 1) {
       return outflows[0].amount;
     }
@@ -72,21 +71,22 @@ const DashboardView = () => {
     history.push(`/budget/${budgetId}`);
   }
 
-  // useEffect(() => {
-  //   let loadedBudget = null;
-  //   const budgets = JSON.parse(localStorage.getItem("budgets"));
-  //   if (budgets) {
-  //     loadedBudget = budgets[`${now.getMonth() + 1}-${now.getFullYear()}`];
-  //     setBudget(loadedBudget);
-  //   }
-  // }, [now]);
+  useEffect(() => {
+    let loadedBudget = null;
+    const budgets = JSON.parse(localStorage.getItem("budgets"));
+    if (budgets) {
+      loadedBudget = budgets[`${now.getMonth() + 1}-${now.getFullYear()}`];
+      setBudget(loadedBudget);
+    }
+  }, [now]);
 
   if (!currentBudget) {
     return (
       <div>
         <h1>
           No budget has been create for this month,{" "}
-          <Link to="/budget/create">Create one?</Link>
+          {/* <Link to="/budget/create">Create one?</Link> */}
+          <button onClick={() => createBlankBudget()}>Create one?</button>
         </h1>
       </div>
     );
